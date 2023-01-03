bcb2c5a60e82ec8b88e8b5e3bd10dc6e011f8f62
tranthaicong2610 <69552670+tranthaicong2610@users.noreply.github.com>
2023-01-03 07:45:10 +0700

    cart - list

--- app/src/app/layout/content-layout/homepage/homepage.module.ts
+++ app/src/app/layout/content-layout/homepage/homepage.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomepageComponent } from './homepage.component';
 import { HomepageRoutingModule } from './homepage-routing.module';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
-  declarations:[HomepageComponent],
+  declarations:[HomepageComponent, ProductListComponent],
   imports: [CommonModule, HomepageRoutingModule],
 })
 export class HomepageModule {}
