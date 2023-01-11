f33060ddaa790a1a2fd227a6e8fad69a8f693856
tranthaicong2610 <69552670+tranthaicong2610@users.noreply.github.com>
2023-01-12 05:49:49 +0700

    order detail

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -7,8 +7,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './layout/content-layout/footer/footer.component';
 import { HeaderComponent } from './layout/content-layout/header/header.component';
+import { OrderDetailComponent } from './layout/content-layout/order-detail/order-detail.component';
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, OrderDetailComponent ],
   imports: [
     BrowserModule,
     AppRoutingModule,
